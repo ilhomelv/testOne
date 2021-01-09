030e669b81b5b920c1c50d720c1f63ab9922cf78
Hooja <34709366+ilhomelv@users.noreply.github.com>
2021-01-09 21:23:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let web3 = new Web3("https://rinkeby.infura.io/v3/db82b7b2e5cf4a46adb6e58f158c3cd6");
 
 
@@ -43,6 +41,30 @@ var contractAddress =   abiJSON.at('0xAff036FA0Cf3c5782ed2c1214AEbBCc6149931fa')
 
 console.log(contractAddress);
 console.log(web3);
+
+
+
+ethereum.request({ method: 'eth_requestAccounts' });
+
+
+
+const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+const account = accounts[0];
+// We currently only ever provide a single account,
+// but the array gives us some room to grow.
+
+
+const showAccount = document.querySelector('.showAccount');
+
+ethereumButton.addEventListener('click', () => {
+  getAccount();
+});
+
+async function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+}
 
 
 
