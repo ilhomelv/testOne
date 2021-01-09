212c16e90a4a7e16f40fe3c1ed9cd250a407c979
Hooja <34709366+ilhomelv@users.noreply.github.com>
2021-01-09 11:03:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
-//let web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/YOUR_TOKEN_HERE"));
+let web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/0x2E2B5026C94dcbA09f6DFC6619845863ABAd1E5E"));
 
-let web3 = new Web3(Web3.givenProvider);
+//let web3 = new Web3(Web3.givenProvider);
 
 
 
