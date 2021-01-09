661e1386ae3310ede51b794704bd6b373d9feacd
Hooja <34709366+ilhomelv@users.noreply.github.com>
2021-01-09 18:20:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
-let web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/db82b7b2e5cf4a46adb6e58f158c3cd6"));
+//let web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/db82b7b2e5cf4a46adb6e58f158c3cd6"));
 
-//let web3 = new Web3(Web3.givenProvider);
+let web3 = new Web3("https://rinkeby.infura.io/v3/db82b7b2e5cf4a46adb6e58f158c3cd6");
 
 
 var abiJSON = web3.eth.contract([
