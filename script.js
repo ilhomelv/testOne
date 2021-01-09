ab7c117508b2587a00113ac80884174df00aa1f1
Hooja <34709366+ilhomelv@users.noreply.github.com>
2021-01-09 18:02:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ var contractAddress =   abiJSON.at('0xAff036FA0Cf3c5782ed2c1214AEbBCc6149931fa')
 
 console.log(contractAddress);
 
+console.log(getCounter());
+
 //console.log(web3);
 
 //console.log(web3.eth.getAccounts);
